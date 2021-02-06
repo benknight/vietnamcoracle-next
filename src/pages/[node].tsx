49fb49125c7c5d097f373b45c0d7b693effdfeab
Benjamin Knight <ben@benknight.me>
2021-02-06 22:26:00 +0700

    Update [node].tsx

--- src/pages/[node].tsx
+++ src/pages/[node].tsx
@@ -21,7 +21,7 @@ function cleanPostHTML(html: string): string {
   result = result.replace(/(http)\:\/\//gm, 'https://');
   // Set language to English on all embeded maps
   result = result.replace(
-    /(google\.com\/maps\/d\/embed([\?&][\w-]+=[\w-]+)+)/g,
+    /(google\.com\/maps\/d\/embed([\?&][\w-\.]+=[\w-\.]+)+)/g,
     '$1&hl=en',
   );
   return result;

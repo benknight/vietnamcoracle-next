49c28bfac8c63c7baab5024cde75943d10008019
Benjamin Knight <ben@benknight.me>
2021-02-08 12:02:51 +0700

    Scroll past header on route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { create } from 'jss';
+import { useEffect, useMemo } from 'react';
 import { StylesProvider, jssPreset } from '@material-ui/core/styles';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
@@ -10,14 +12,38 @@ const PreviewAlert = dynamic(() => import('../components/PreviewAlert'));
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { preview } = pageProps;
-  const jss = create({
-    ...jssPreset(),
-    // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
-    insertionPoint:
-      typeof window !== 'undefined'
-        ? window.document.getElementById('jss-insertion-point')
-        : undefined,
-  });
+  const router = useRouter();
+  const jss = useMemo(
+    () =>
+      create({
+        ...jssPreset(),
+        // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
+        insertionPoint:
+          typeof window !== 'undefined'
+            ? window.document.getElementById('jss-insertion-point')
+            : undefined,
+      }),
+    [],
+  );
+
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      window.requestAnimationFrame(() => {
+        if (window.scrollY < 317) {
+          window.scrollTo(0, 317);
+        }
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <StylesProvider jss={jss}>
       {preview && <PreviewAlert />}

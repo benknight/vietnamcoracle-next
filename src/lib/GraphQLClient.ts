9e4c718c25484a90968b4b1c7bcf14745a294c26
Benjamin Knight <ben@benknight.me>
2021-03-24 22:43:35 +0700

    Update GraphQLClient.ts

--- src/lib/GraphQLClient.ts
+++ src/lib/GraphQLClient.ts
@@ -1,11 +1,14 @@
 import { GraphQLClient } from 'graphql-request';
 
-const client = new GraphQLClient(process.env.NEXT_PUBLIC_WORDPRESS_API_URL, {
-  headers: {
-    Authorization: process.env.WORDPRESS_AUTH_REFRESH_TOKEN
-      ? `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`
-      : null,
+const client = new GraphQLClient(
+  `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql`,
+  {
+    headers: {
+      Authorization: process.env.WORDPRESS_AUTH_REFRESH_TOKEN
+        ? `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`
+        : null,
+    },
   },
-});
+);
 
 export default client;

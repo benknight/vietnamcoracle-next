cfb90f749c18fa36fac380286692820ea4efe0cf
Benjamin Knight <ben@benknight.me>
2021-03-25 19:31:51 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {

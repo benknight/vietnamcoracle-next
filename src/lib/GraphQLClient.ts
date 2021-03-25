5a43e16b244a5c897324f1479616ae3a14687a2d
Benjamin Knight <ben@benknight.me>
2021-03-25 12:54:47 +0700

    Update GraphQLClient.ts

--- src/lib/GraphQLClient.ts
+++ src/lib/GraphQLClient.ts
@@ -1,14 +1,25 @@
 import { GraphQLClient } from 'graphql-request';
 
+const config = {
+  headers: {},
+};
+
+if (
+  typeof window === 'undefined' &&
+  process.env.WORDPRESS_API_USERNAME &&
+  process.env.WORDPRESS_API_PASSWORD
+) {
+  config.headers = {
+    Authorization: `Basic ${Buffer.from(
+      `${process.env.WORDPRESS_API_USERNAME}:${process.env.WORDPRESS_API_PASSWORD}`,
+      'utf-8',
+    ).toString('base64')}`,
+  };
+}
+
 const client = new GraphQLClient(
   `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql`,
-  {
-    headers: {
-      Authorization: process.env.WORDPRESS_AUTH_REFRESH_TOKEN
-        ? `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`
-        : null,
-    },
-  },
+  config,
 );
 
 export default client;

2e0c01c73d7a90cca7630610075c89cab76b6090
Benjamin Knight <ben@benknight.me>
2021-03-30 22:02:59 +0700

    Show loading indicators during page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
+import cx from 'classnames';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import { create } from 'jss';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import { StylesProvider, jssPreset } from '@material-ui/core/styles';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import useWaitCursor from '../lib/useWaitCursor';
 import '../styles/style.css';
 
 const PreviewAlert = dynamic(() => import('../components/PreviewAlert'));
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { preview } = pageProps;
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const jss = useMemo(
     () =>
       create({
@@ -24,8 +30,33 @@ function MyApp({ Component, pageProps }: AppProps) {
     [],
   );
 
+  useEffect(() => {
+    const routeChangeStart = () => setLoading(true);
+    const routeChangeComplete = () => setLoading(false);
+    const routeChangeError = () => setLoading(false);
+    router.events.on('routeChangeStart', routeChangeStart);
+    router.events.on('routeChangeComplete', routeChangeComplete);
+    router.events.on('routeChangeError', routeChangeError);
+    return () => {
+      router.events.off('routeChangeStart', routeChangeStart);
+      router.events.off('routeChangeComplete', routeChangeComplete);
+      router.events.off('routeChangeError', routeChangeError);
+    };
+  }, []);
+
+  useWaitCursor(loading);
+
   return (
     <StylesProvider jss={jss}>
+      <LinearProgress
+        classes={{
+          root: cx('absolute z-40 top-0 w-full h-0.5 bg-transparent', {
+            'block lg:hidden': loading,
+            hidden: !loading,
+          }),
+          bar: 'bg-gray-700 dark:bg-white',
+        }}
+      />
       {preview && <PreviewAlert />}
       <Header preview={preview} />
       <Nav preview={preview} />

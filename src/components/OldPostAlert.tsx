a46bea11ef73f405a8528372e4f2aaddf71de7e8
Benjamin Knight <ben@benknight.me>
2021-05-10 15:16:18 +0700

    Use fossil icon

--- src/components/OldPostAlert.tsx
+++ src/components/OldPostAlert.tsx
@@ -1,18 +1,24 @@
 // import Link from 'next/link';
-import { ClockIcon } from '@heroicons/react/outline';
+// @ts-ignore
+import FossilIcon from '../../public/fossil.svg';
 
 export default function OldPostAlert({ monthsOld = 0 }) {
   return (
-    <div className="flex items-center py-4 px-2 mb-4 mt-3 rounded bg-yellow-100 dark:bg-yellow-900  dark:bg-opacity-25 dark:border dark:border-yellow-600">
-      <ClockIcon className="self-start flex-shrink-0 w-6 h-6 ml-1 mr-2 text-yellow-600 dark:text-yellow-500" />
-      <div className="text-xs sm:text-sm flex-auto leading-tight">
-        <p>
+    <div className="flex items-center py-4 px-2 mb-4 mt-3 rounded bg-yellow-100 bg-opacity-50 dark:bg-yellow-900  dark:bg-opacity-25 border border-yellow-100 dark:border-yellow-900">
+      <span title="Science magnifying glass fossil by Maxicons from the Noun Project">
+        <FossilIcon className="self-start flex-shrink-0 w-16 h-16 ml-1 mr-2 -mb-2" />
+      </span>
+      <div className="flex-auto leading-tight">
+        <p className="text-base">
           Heads up, this post was last updated more than{' '}
-          {Math.floor(monthsOld / 12)} years ago. Check the{' '}
+          {Math.floor(monthsOld / 12)} years ago.
+        </p>
+        <p className="text-xs sm:text-sm">
+          Check the{' '}
           <a className="link" href="#comments">
-            comments
+            comments section
           </a>{' '}
-          section below for possible updates.
+          below for possible updates.
           {/* <Link href="/updates-and-accuracy">
             <a className="block mt-2 md:inline md:mt-0 link">
               Read more about accuracy &amp; updates

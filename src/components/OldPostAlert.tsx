cff95b63d69b4e9f0188ec0be82a5c02204e0b47
Benjamin Knight <ben@benknight.me>
2021-05-16 23:21:03 +0700

    Update OldPostAlert.tsx

--- src/components/OldPostAlert.tsx
+++ src/components/OldPostAlert.tsx
@@ -1,10 +1,17 @@
+import cx from 'classnames';
 // import Link from 'next/link';
 // @ts-ignore
 import FossilIcon from '../../public/fossil.svg';
 
 export default function OldPostAlert({ monthsOld = 0 }) {
   return (
-    <div className="flex items-center py-4 px-2 mb-4 mt-3 rounded bg-yellow-100 bg-opacity-50 dark:bg-yellow-500 dark:bg-opacity-10 border border-yellow-100 dark:border-yellow-900">
+    <div
+      className={cx(
+        'flex items-center py-4 px-2 mb-4 mt-3',
+        'dark:text-yellow-400 dark:text-opacity-75 rounded',
+        'bg-yellow-100 bg-opacity-50 dark:bg-yellow-500 dark:bg-opacity-10',
+        'border border-yellow-300 dark:border-yellow-500 dark:border-opacity-30',
+      )}>
       <span title="Science magnifying glass fossil by Maxicons from the Noun Project">
         <FossilIcon className="self-start flex-shrink-0 w-16 h-16 ml-1 mr-2 -mb-2" />
       </span>

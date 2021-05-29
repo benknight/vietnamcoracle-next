03f2cc1331a969a0f8f7f73cee9a0c6155ab07de
Benjamin Knight <ben@benknight.me>
2021-05-29 22:22:10 +0700

    Create 500.tsx

---
+++ src/pages/500.tsx
@@ -0,0 +1,9 @@
+export default function InternalServerError() {
+  return (
+    <div className="mx-auto py-48 max-w-screen-lg page-wrap font-display text-center">
+      <h1 className="text-3xl mb-2 text-center">Server Error</h1>
+      Sorry, the site is experiencing technical difficulties. Please check again
+      back later.
+    </div>
+  );
+}

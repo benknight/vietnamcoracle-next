64cd560592c6587fa599fe1fa277023bab716bdb
Benjamin Knight <ben@benknight.me>
2021-05-30 21:12:17 +0700

    More preview fixes

--- src/components/PreviewAlert.tsx
+++ src/components/PreviewAlert.tsx
@@ -4,7 +4,8 @@ import { useRouter } from 'next/router';
 export default function PreviewAlert() {
   const router = useRouter();
   return (
-    <Link href={`/api/exit-preview?redirect=${router.asPath}`}>
+    <Link
+      href={`/api/exit-preview?redirect=${encodeURIComponent(router.asPath)}`}>
       <a className="z-50 fixed flex items-center justify-center h-6 top-0 left-0 w-full bg-blue-500 dark:bg-blue-900 text-white text-xs hover:underline">
         Preview mode enabled. Click here to exit.
       </a>

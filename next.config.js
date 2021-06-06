a58e8713b261d589e840a694eb92dab9b0f36653
Benjamin Knight <ben@benknight.me>
2021-06-06 19:57:55 +0700

    Copy over redirections from plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { redirects } = require('./redirection-plugin-export.json');
+
 module.exports = {
   future: {
     // NOTE(2021-05-12): Disabling due to build issues
@@ -13,6 +15,19 @@ module.exports = {
         permanent: true,
         source: '/category/features-guides/:path*',
       },
+      {
+        destination: '/browse/:path*',
+        permanent: true,
+        source: '/category/:path*',
+      },
+      ...redirects.map(config => ({
+        destination: config.action_data.url.replace(
+          /https?\:\/\/(www\.)?vietnamcoracle\.com/g,
+          '',
+        ),
+        permanent: true,
+        source: config.match_url,
+      })),
     ];
   },
   async rewrites() {

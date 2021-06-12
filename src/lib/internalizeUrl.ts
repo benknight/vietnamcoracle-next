7c8d59c18fb396e598214038eaa2525200d1428f
Benjamin Knight <ben@benknight.me>
2021-06-12 22:37:26 +0700

    Update internalizeUrl.ts

--- src/lib/internalizeUrl.ts
+++ src/lib/internalizeUrl.ts
@@ -1,5 +1,6 @@
 export default function internalizeUrl(url: string): string {
   return url
-    .replace(/https:\/\/vietnamcoracle\.com\//, '')
-    .replace(/https:\/\/www\.vietnamcoracle\.com\//, '');
+    .replace(/https:\/\/vietnamcoracle\.com/, '')
+    .replace(/https:\/\/www\.vietnamcoracle\.com/, '')
+    .replace(/\/$/, '');
 }

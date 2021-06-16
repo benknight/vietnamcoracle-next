b8155a847aab209416459d765e8173ec010a3e91
Benjamin Knight <ben@benknight.me>
2021-06-16 23:18:55 +0700

    Update internalizeUrl.ts

--- src/lib/internalizeUrl.ts
+++ src/lib/internalizeUrl.ts
@@ -1,4 +1,7 @@
 export default function internalizeUrl(url: string): string {
+  if (url.indexOf('/wp-content') > -1) {
+    return url;
+  }
   return url
     .replace(/https:\/\/vietnamcoracle\.com/, '')
     .replace(/https:\/\/www\.vietnamcoracle\.com/, '')

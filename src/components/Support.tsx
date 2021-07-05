fa1969e94733a6a1e59adafe9be3b24e05139d15
Benjamin Knight <ben@benknight.me>
2021-07-05 17:40:04 +0700

    Support design tweaks

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import internalizeUrl from '../lib/internalizeUrl';
+import { CurrencyPoundIcon } from '@heroicons/react/solid';
 import Block, { BlockContent, BlockTitle, BlockType } from './Block';
 
 interface Props {
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const btnClassName =
-  'flex items-center justify-center h-10 mt-2 border bg-gray-100 dark:bg-opacity-10 dark:hover:bg-opacity-50 dark:text-white hover:text-white rounded-full transition-colors duration-100 ease-in-out';
+  'flex items-center justify-center mt-2 border bg-gray-100 dark:bg-opacity-10 hover:bg-opacity-25 dark:hover:bg-opacity-33 dark:text-white rounded-xl transition-colors duration-100 ease-in-out';
 
 export default function Support({ data: block }: Props) {
   return (
@@ -23,33 +23,34 @@ export default function Support({ data: block }: Props) {
         </p>
       </BlockContent>
       <div className="max-w-xs mx-auto text-sm tracking-wider">
+        <Link href="/donations-page">
+          <a
+            className={`${btnClassName} border-white dark:border-blue-400 dark:border-opacity-50 bg-blue-400 hover:bg-opacity-100 dark:hover:bg-opacity-75 text-white h-12`}>
+            <CurrencyPoundIcon className="w-5 h-5 mr-1" />{' '}
+            <span className="pr-4">Donate</span>
+          </a>
+        </Link>
         <a
-          className={`${btnClassName} bg-[#FF424D] !border-[#FF424D] text-white dark:hover:bg-opacity-100`}
+          className={`${btnClassName} border-white dark:border-black hover:bg-[#FF424D] h-10`}
           href="https://www.patreon.com/vietnamcoracle"
           target="_blank">
           <img
             alt=""
-            className="w-5 h-5 mr-2"
-            src="/Digital-Patreon-Logo_White.png"
+            className="w-4 h-4 mr-2"
+            src="/Digital-Patreon-Logo_FieryCoral.png"
           />
           Become a Patron
         </a>
-        <Link href="/donations-page">
-          <a
-            className={`${btnClassName} border-white dark:border-black hover:bg-purple-400`}>
-            Donate
-          </a>
-        </Link>
         <div className="flex text-xs">
           <Link href="/book-your-accommodation">
             <a
-              className={`${btnClassName} flex-auto border-white dark:border-black hover:bg-blue-400`}>
+              className={`${btnClassName} border-white dark:border-black flex-auto hover:bg-purple-400 h-10`}>
               Book Hotels
             </a>
           </Link>
           <Link href="/book-your-transportation">
             <a
-              className={`${btnClassName} ml-1 flex-auto border-white dark:border-black hover:bg-green-400`}>
+              className={`${btnClassName} border-white dark:border-black ml-1 flex-auto hover:bg-green-400 h-10`}>
               Book Transportation
             </a>
           </Link>

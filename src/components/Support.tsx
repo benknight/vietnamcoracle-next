4b9f2f43cd259e6ebfb7fb1586a122b897223ef0
Benjamin Knight <ben@benknight.me>
2021-07-06 15:15:21 +0700

    Update Support.tsx

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import internalizeUrl from '../lib/internalizeUrl';
-import { CurrencyPoundIcon } from '@heroicons/react/solid';
+import { CurrencyDollarIcon } from '@heroicons/react/solid';
 import Block, { BlockContent, BlockTitle, BlockType } from './Block';
 
 interface Props {
@@ -25,8 +25,8 @@ export default function Support({ data: block }: Props) {
       <div className="max-w-xs mx-auto text-sm tracking-wider">
         <Link href="/donations-page">
           <a
-            className={`${btnClassName} border-white dark:border-blue-400 dark:border-opacity-50 bg-blue-400 hover:bg-opacity-100 dark:hover:bg-opacity-75 text-white h-12`}>
-            <CurrencyPoundIcon className="w-5 h-5 mr-1" />{' '}
+            className={`${btnClassName} border-white dark:border-purple-400 dark:border-opacity-50 bg-purple-400 hover:bg-opacity-100 dark:hover:bg-opacity-75 text-white h-12`}>
+            <CurrencyDollarIcon className="w-5 h-5 mr-1" />{' '}
             <span className="pr-4">Donate</span>
           </a>
         </Link>
@@ -44,14 +44,14 @@ export default function Support({ data: block }: Props) {
         <div className="flex text-xs">
           <Link href="/book-your-accommodation">
             <a
-              className={`${btnClassName} border-white dark:border-black flex-auto hover:bg-purple-400 h-10`}>
-              Book Hotels
+              className={`${btnClassName} border-white dark:border-black flex-auto hover:bg-blue-400 h-10`}>
+              🛌 Book Hotels
             </a>
           </Link>
           <Link href="/book-your-transportation">
             <a
               className={`${btnClassName} border-white dark:border-black ml-1 flex-auto hover:bg-green-400 h-10`}>
-              Book Transportation
+              ✈️ Book Transportation
             </a>
           </Link>
         </div>

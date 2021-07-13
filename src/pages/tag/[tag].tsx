efb3653a310de4790a856bc37f0a76a4fdab59bd
Benjamin Knight <ben@benknight.me>
2021-07-13 18:22:01 +0700

    Create [tag].tsx

---
+++ src/pages/tag/[tag].tsx
@@ -0,0 +1,129 @@
+import { gql } from 'graphql-request';
+import htmlToReact from 'html-react-parser';
+import _ from 'lodash';
+import type { GetStaticPaths, InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
+import Footer from '../../components/Footer';
+import Hero, { HeroContent } from '../../components/Hero';
+import Layout, { LayoutMain, LayoutSidebar } from '../../components/Layout';
+import PostCard, { SwatchesProvider } from '../../components/PostCard';
+import SidebarDefault from '../../components/SidebarDefault';
+import GraphQLClient from '../../lib/GraphQLClient';
+import generateSwatches from '../../lib/generateSwatches';
+
+const Tag = ({
+  data,
+  swatches,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const coverImgSm = data.tag.cover?.small || data.defaultImages?.cover.small;
+  const coverImgLg = data.tag.cover?.large || data.defaultImages?.cover.large;
+  return (
+    <SwatchesProvider value={swatches}>
+      <Head>{htmlToReact(data.tag.seo.fullHead)}</Head>
+      <Hero imgSm={coverImgSm} imgLg={coverImgLg} theme="dark">
+        <HeroContent>
+          <div className="page-wrap">
+            <h1 className="font-display text-2xl md:text-3xl lg:text-3xl leading-tight">
+              Posts tagged <i>“{data.tag.name}”</i>
+            </h1>
+          </div>
+        </HeroContent>
+      </Hero>
+      <Layout>
+        <LayoutMain>
+          <div className="page-wrap pt-4 xl:pr-0 grid gap-4 xl:gap-6 md:grid-cols-2">
+            {data.tag.posts.nodes.map(post => (
+              <PostCard key={post.slug} post={post} inGrid />
+            ))}
+          </div>
+        </LayoutMain>
+        <LayoutSidebar>
+          <SidebarDefault data={data} />
+          <Footer data={data} />
+        </LayoutSidebar>
+      </Layout>
+    </SwatchesProvider>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const query = gql`
+    {
+      tags(first: 1000) {
+        nodes {
+          uri
+        }
+      }
+    }
+  `;
+  const data = await GraphQLClient.request(query);
+  const result = {
+    paths: [
+      ...data.tags.nodes.map(tag => ({
+        params: { tag: tag.uri.split('/').filter(token => Boolean(token))[1] },
+      })),
+    ],
+    fallback: false,
+  };
+  return result;
+};
+
+export const getStaticProps = async ({ params, preview = false }) => {
+  const query = gql`
+    query Tag($preview: Boolean!, $slug: ID!) {
+      tag(id: $slug, idType: SLUG) {
+        name
+        slug
+        cover {
+          small {
+            ...HeroImageData
+          }
+          large {
+            ...HeroImageData
+          }
+        }
+        posts(first: 1000) {
+          nodes {
+            slug
+            ...PostCardPostData
+          }
+        }
+        seo {
+          fullHead
+        }
+      }
+      defaultImages {
+        cover {
+          large {
+            ...HeroImageData
+          }
+          small {
+            ...HeroImageData
+          }
+        }
+      }
+      ...FooterData
+      ...SidebarDefaultData
+    }
+    ${Footer.fragments}
+    ${Hero.fragments}
+    ${PostCard.fragments}
+    ${SidebarDefault.fragments}
+  `;
+
+  const data = await GraphQLClient.request(query, {
+    preview,
+    slug: params.tag,
+  });
+
+  return {
+    props: {
+      data,
+      preview,
+      swatches: await generateSwatches(JSON.stringify(data)),
+    },
+    revalidate: 1,
+  };
+};
+
+export default Tag;

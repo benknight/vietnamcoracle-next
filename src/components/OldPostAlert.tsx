56c9d662088dac3387ed73fa232120a05343b3b7
Benjamin Knight <ben@benknight.me>
2021-08-04 14:24:29 -0400

    Update OldPostAlert.tsx

--- src/components/OldPostAlert.tsx
+++ src/components/OldPostAlert.tsx
@@ -10,25 +10,24 @@ export default function OldPostAlert({ className = '', monthsOld = 0 }) {
         className,
         'flex items-center py-4 px-2 my-2',
         'dark:text-yellow-200 dark:text-opacity-75 rounded',
-        'bg-yellow-100 bg-opacity-50 dark:bg-yellow-500 dark:bg-opacity-10',
+        'bg-yellow-100 bg-opacity-50 dark:bg-transparent',
         'border border-yellow-300 dark:border-yellow-500 dark:border-opacity-30',
       )}>
       <Icon className="fill-current self-start flex-shrink-0 w-12 h-12 mr-2 -mb-2 md:ml-1" />
       <div className="flex-auto">
         <p className="text-[12px] leading-normal sm:text-sm xl:tracking-wide">
           <b className="font-bold">
-            This post was last updated more than {Math.floor(monthsOld / 12)}{' '}
-            years ago.
+            This post was last updated {Math.floor(monthsOld / 12)} years ago.
           </b>{' '}
           Please{' '}
           <a className="link" href="#comments">
             check the comments
           </a>{' '}
-          section below for possible updates, or{' '}
+          section for possible updates, or read more on my{' '}
           <Link href="/updates-accuracy">
-            <a className="link">read more</a>
-          </Link>{' '}
-          about updates &amp; accuracy.
+            <a className="link">Updates &amp; Accuracy page</a>
+          </Link>
+          .
         </p>
       </div>
     </div>

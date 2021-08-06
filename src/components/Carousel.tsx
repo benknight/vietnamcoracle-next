10e06e4b484e774a94f93a41d4ebd558e1537497
Benjamin Knight <ben@benknight.me>
2021-08-06 13:30:30 -0400

    Carousel improvements - autoplay, play buttons

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,13 +1,75 @@
 import cx from 'classnames';
-import { useRef, useState, useEffect } from 'react';
+import { useCallback, useRef, useState, useEffect } from 'react';
+import { RadioGroup } from '@headlessui/react';
+import PauseIcon from '@material-ui/icons/PauseRounded';
+import PlayArrowIcon from '@material-ui/icons/PlayArrowRounded';
+import SkipNextIcon from '@material-ui/icons/SkipNextRounded';
+import SkipPrevIcon from '@material-ui/icons/SkipPreviousRounded';
+import PauseRounded from '@material-ui/icons/PauseRounded';
+
+let advance = () => {};
+
+const navIconClassName = '!w-6 !h-6';
 
 export default function Carousel({ className, children, ...props }) {
-  const ref = useRef<HTMLDivElement>();
+  const rootRef = useRef<HTMLDivElement>();
+  const intervalRef = useRef<number>();
+  const [play, setPlay] = useState(true);
   const [slideCount, setSlideCount] = useState(null);
   const [cursor, setCursor] = useState(0);
 
+  const goTo = useCallback(
+    (destination: number, intent: 'auto' | 'manual' = 'manual') => {
+      const slides = rootRef.current?.querySelectorAll(':scope > a');
+      if (intent === 'manual') {
+        setPlay(false);
+      }
+      rootRef.current.scrollTo({
+        left: (slides[destination] as HTMLElement).offsetLeft,
+        behavior: intent === 'auto' ? 'smooth' : 'auto',
+      });
+    },
+    [],
+  );
+
   useEffect(() => {
-    const slides = ref.current.querySelectorAll(':scope > a');
+    // Auto-play
+    if (!slideCount) return;
+    if (play && !intervalRef.current) {
+      // Advance immediately if the carousel was previously paused
+      if (intervalRef.current === null) {
+        advance();
+      }
+      intervalRef.current = window.setInterval(() => {
+        advance();
+      }, 5000);
+    }
+    if (!play && intervalRef.current) {
+      window.clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  }, [play, slideCount]);
+
+  useEffect(() => {
+    // Update the advance callback
+    advance = () => goTo((cursor + 1) % slideCount, 'auto');
+  }, [cursor, slideCount]);
+
+  useEffect(() => {
+    // Pause when the user pans left/right
+    const onWheel = (event: WheelEvent) => {
+      // TODO: Improve this logic
+      if (Math.abs(event.deltaX) / Math.min(1, Math.abs(event.deltaY)) > 1) {
+        setPlay(false);
+      }
+    };
+    rootRef.current?.addEventListener('wheel', onWheel);
+    return () => rootRef.current?.removeEventListener('wheel', onWheel);
+  }, []);
+
+  useEffect(() => {
+    // Update cursor when user scrolls
+    const slides = rootRef.current.querySelectorAll(':scope > a');
     setSlideCount(slides.length);
     const observer = new IntersectionObserver(
       entries => {
@@ -17,12 +79,11 @@ export default function Carousel({ className, children, ...props }) {
             let child = entry.target;
             while ((child = child.previousSibling as HTMLElement)) index++;
             setCursor(index);
-            // console.log('setCursor', index);
           }
         });
       },
       {
-        root: ref.current,
+        root: rootRef.current,
         threshold: 0.5,
       },
     );
@@ -39,47 +100,84 @@ export default function Carousel({ className, children, ...props }) {
   }, []);
 
   return (
-    <div {...props} className={cx(className, 'relative overflow-hidden')}>
-      <div
-        className="snap snap-mandatory snap-x overflow-x-auto flex flex-nowrap w-full h-full -mb-4 pb-14"
-        dir="ltr"
-        ref={ref}>
-        {children}
+    <div {...props} className={cx(className, 'group')}>
+      <div className="relative overflow-hidden">
+        <div
+          className="relative snap snap-mandatory snap-x overflow-x-auto flex flex-nowrap w-full h-full"
+          dir="ltr"
+          ref={rootRef}>
+          {children}
+        </div>
+        {slideCount > 0 && (
+          <nav className="hidden pointer:flex h-10 absolute left-1/2 top-full transform -translate-x-1/2 transition duration-300 ease opacity-0 group-hover:-translate-y-16 group-hover:opacity-100 bg-black text-white shadow-xl rounded-full">
+            <button
+              aria-title="Prev"
+              className="flex items-center pl-2 pr-1 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500"
+              onClick={() => {
+                setPlay(false);
+                goTo((cursor - 1 + slideCount) % slideCount, 'auto');
+              }}>
+              <SkipPrevIcon className={navIconClassName} />
+            </button>
+            <button
+              aria-title={play ? 'Pause' : 'Play'}
+              className="flex items-center px-1 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500"
+              onClick={() => setPlay(play => !play)}>
+              {play ? (
+                <PauseIcon className={navIconClassName} />
+              ) : (
+                <PlayArrowIcon className={navIconClassName} />
+              )}
+            </button>
+            <button
+              aria-title="Next"
+              className="flex items-center pl-1 pr-2 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500"
+              onClick={() => {
+                setPlay(false);
+                goTo((cursor + 1) % slideCount, 'auto');
+              }}>
+              <SkipNextIcon className={navIconClassName} />
+            </button>
+          </nav>
+        )}
       </div>
       {(() => {
         if (slideCount === 0) return null;
         const buttons = [];
         for (let i = 0; i < slideCount; i++) {
           buttons.push(
-            <button
-              className="pointer-only p-1"
+            <RadioGroup.Option
+              className="cursor-pointer pointer-only p-1"
               key={i}
-              onClick={() => {
-                const slides = ref.current.querySelectorAll(':scope > a');
-                slides[i].scrollIntoView({ behavior: 'smooth' });
-                setCursor(i);
-              }}>
-              <span
-                className={cx(
-                  'block bg-primary-700 dark:bg-white rounded-full shadow',
-                  {
-                    'w-2 h-2': cursor === i,
-                    'w-1 h-1 border border-primary-700 dark:border-white opacity-50 box-content':
-                      cursor !== i,
-                  },
-                )}
-              />
-              <span className="sr-only">{i}</span>
-            </button>,
+              value={i}>
+              {({ checked }) => (
+                <>
+                  <span
+                    className={cx(
+                      'block bg-primary-700 dark:bg-white rounded-full shadow',
+                      {
+                        'w-2 h-2': checked,
+                        'w-1 h-1 border border-primary-700 dark:border-white opacity-50 box-content':
+                          !checked,
+                      },
+                    )}
+                  />
+                  <span className="sr-only">{i}</span>
+                </>
+              )}
+            </RadioGroup.Option>,
           );
         }
         return (
-          <div
-            className="
-              py-2 w-full flex justify-center items-center
-              absolute bottom-0 left-0 cursor-default">
+          <RadioGroup
+            className="py-2 w-full flex justify-center items-center cursor-default"
+            value={cursor}
+            onChange={i => goTo(i)}>
+            <RadioGroup.Label className="sr-only">
+              Slider Image Index
+            </RadioGroup.Label>
             {buttons}
-          </div>
+          </RadioGroup>
         );
       })()}
     </div>

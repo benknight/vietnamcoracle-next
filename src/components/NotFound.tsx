24ad5791ccb010aa2da4108934bfb8cbd32f7520
Benjamin Knight <ben@benknight.me>
2021-08-23 23:30:24 -0400

    Update NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   const router = useRouter();
   const emojiClassName = 'pl-4 sm:pl-16 text-4xl leading-normal text-gray-300';
   return (
-    <div className="page-wrap page-wrap--center">
+    <div className="page-wrap page-wrap--center font-display">
       <div className={emojiClassName}>👀…</div>
       <div className="py-4 text-center">
         <h1 className="text-3xl mb-2 text-center">Page Not Found</h1>

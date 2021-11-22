6449db839ad847cb038cbd053c6119a69d6a0b5c
Benjamin Knight <ben@benknight.me>
2021-11-21 21:15:06 -0500

    Fix #map scroll-margin-top

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,55 +8,52 @@ const Map = ({ data }) => {
   const router = useRouter();
   const { browse } = router.query;
   return (
-    <div className="lg:rounded-lg overflow-hidden">
-      <div
-        className={cx(
-          'relative py-8 px-6 md:p-8 text-center dark:bg-opacity-10 dark:text-white lg:rounded-t-lg font-display',
-          {
-            'bg-blue-100 dark:bg-blue-500': !browse,
-            'bg-amber-100 dark:bg-amber-400': browse?.[0] === 'hotel-reviews',
-            'text-white bg-green-600': browse?.[0] === 'motorbike-guides',
-            'text-white bg-yellow-900': browse?.[0] === 'destinations',
-            'bg-yellow-200': browse?.[0] === 'food-and-drink',
-          },
-        )}>
-        <h3
-          className="text-xl xs:text-2xl sm:text-3xl"
-          id="map"
-          style={{ scrollMarginTop: '2rem' }}>
-          {data.title}
-        </h3>
-        <p className="max-w-4xl mt-2 mb-4 sm:px-16 mx-auto text-sm md:text-base font-serif dark:text-gray-400">
-          {data.description}
-        </p>
-        <a
-          className="
+    <div id="map">
+      <div className="lg:rounded-lg overflow-hidden">
+        <div
+          className={cx(
+            'relative py-8 px-6 md:p-8 text-center dark:bg-opacity-10 dark:text-white lg:rounded-t-lg font-display',
+            {
+              'bg-blue-100 dark:bg-blue-500': !browse,
+              'bg-amber-100 dark:bg-amber-400': browse?.[0] === 'hotel-reviews',
+              'text-white bg-green-600': browse?.[0] === 'motorbike-guides',
+              'text-white bg-yellow-900': browse?.[0] === 'destinations',
+              'bg-yellow-200': browse?.[0] === 'food-and-drink',
+            },
+          )}>
+          <h3 className="text-xl xs:text-2xl sm:text-3xl">{data.title}</h3>
+          <p className="max-w-4xl mt-2 mb-4 sm:px-16 mx-auto text-sm md:text-base font-serif dark:text-gray-400">
+            {data.description}
+          </p>
+          <a
+            className="
             px-3 py-2 inline-flex items-center
             text-sm sm:text-base font-sans hover:underline
             bg-white bg-opacity-90 text-gray-800 rounded-full shadow-md"
-          href={`https://www.google.com/maps/d/viewer?mid=${data.mid}`}>
-          <div className="inline-flex mr-2">
-            <Image
-              alt=""
-              height="24"
-              layout="fixed"
-              src="/google-maps-logo.svg"
-              width="24"
-            />
-          </div>
-          Open in Google Maps
-          <ExternalLinkIcon className="ml-2 w-4 h-4" />
-        </a>
-      </div>
-      <div className="relative">
-        <div className="overflow-hidden">
-          <div className="h-[75vh] mb-[-14px] min-h-[850px]">
-            <iframe
-              className="absolute inset-0 w-full h-full"
-              height="650"
-              src={`https://www.google.com/maps/d/embed?mid=${data.mid}&z=6&hl=en`}
-              title={data.title}
-              width="800"></iframe>
+            href={`https://www.google.com/maps/d/viewer?mid=${data.mid}`}>
+            <div className="inline-flex mr-2">
+              <Image
+                alt=""
+                height="24"
+                layout="fixed"
+                src="/google-maps-logo.svg"
+                width="24"
+              />
+            </div>
+            Open in Google Maps
+            <ExternalLinkIcon className="ml-2 w-4 h-4" />
+          </a>
+        </div>
+        <div className="relative">
+          <div className="overflow-hidden">
+            <div className="h-[75vh] mb-[-14px] min-h-[850px]">
+              <iframe
+                className="absolute inset-0 w-full h-full"
+                height="650"
+                src={`https://www.google.com/maps/d/embed?mid=${data.mid}&z=6&hl=en`}
+                title={data.title}
+                width="800"></iframe>
+            </div>
           </div>
         </div>
       </div>

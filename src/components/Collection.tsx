cd779d18652fa0dc79368d8996ab1884e17ff3f4
Benjamin Knight <ben@benknight.me>
2022-04-06 23:00:39 -0300

    Update Collection.tsx

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,6 +1,6 @@
 import cx from 'classnames';
 import _shuffle from 'lodash/shuffle';
-import { RefObject } from 'react';
+import { RefObject, useMemo } from 'react';
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/outline';
 import useCarousel from '../lib/useCarousel';
 import PostCard from './PostCard';
@@ -15,6 +15,18 @@ const Collection = ({ ad, data }) => {
   } = useCarousel();
   const navButtonClassName =
     'flex items-center justify-center absolute z-10 top-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-black hover:bg-blue-400 transition-colors duration-50 ease-out bg-opacity-75 text-white top-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-black hover:bg-blue-400 transition-colors duration-50 ease-out bg-opacity-75 text-white';
+  const items = useMemo(() => {
+    if (!ad.enabled) return data.posts;
+    const mapPosts = post => ({
+      type: 'post',
+      data: post,
+    });
+    return [
+      ...data.posts.slice(0, ad.position - 1).map(mapPosts),
+      { type: 'ad', data: ad },
+      ...data.posts.slice(ad.position - 1).map(mapPosts),
+    ];
+  }, [ad, data.posts]);
   return (
     <div className="relative my-3 lg:my-4 overflow-hidden">
       {!isTouchDevice && scrollPosition && (
@@ -43,14 +55,14 @@ const Collection = ({ ad, data }) => {
         <ol
           className="flex pb-8 -mb-8 pl-4 md:pl-8 overflow-y-auto"
           ref={scrollAreaRef as RefObject<HTMLOListElement>}>
-          {data.posts?.map((post, index) => (
+          {items?.map((item, index) => (
             <li
               className="flex shrink-0 w-4/5 sm:w-3/7 lg:w-3/8 xl:w-3/7 xl:min-w-[23rem] xl:max-w-[25rem] pr-3 lg:pr-4 xl:pr-3 2xl:pr-6"
-              key={post.slug}>
+              key={index}>
               <PostCard
-                {...(ad?.enabled && index === ad.position - 1
-                  ? { ad }
-                  : { post })}
+                {...(item.type === 'ad'
+                  ? { ad: item.data }
+                  : { post: item.data })}
               />
             </li>
           ))}

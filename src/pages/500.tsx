997baaa835423378643961925328950ba1b2b817
Benjamin Knight <ben@benknight.me>
2022-08-11 15:06:54 +0700

    Update 500.tsx

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,8 +2,7 @@ export default function InternalServerError() {
   return (
     <div className="mx-auto py-48 max-w-screen-lg page-wrap font-display text-center">
       <h1 className="text-3xl mb-2 text-center">Server Error</h1>
-      Sorry, the site is experiencing technical difficulties. Please check again
-      back later.
+      Sorry, the site is experiencing technical difficulties. Please check back again later.
     </div>
   );
 }

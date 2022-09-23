0ae69a557124f02b0bb92eb1c05363f09822d59f
Benjamin Knight <ben@benknight.me>
2022-09-23 23:04:11 +0700

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,19 @@
 import cx from 'classnames';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { ExternalLinkIcon } from '@heroicons/react/solid';
 
 const Map = ({ data }) => {
   const router = useRouter();
+  const [mapInteractive, setMapInteractive] = useState(false);
   const { browse } = router.query;
   return (
     <div id="map">
       <div className="lg:rounded-lg overflow-hidden">
         <div
           className={cx(
-            'relative py-8 px-6 md:p-8 text-center dark:bg-opacity-10 dark:text-white lg:rounded-t-lg font-display',
+            'relative py-8 px-6 md:p-8 text-center dark:bg-opacity-25 dark:text-white lg:rounded-t-lg font-display',
             {
               'bg-blue-100 dark:bg-blue-500': browse === 'features-guides',
               'bg-amber-100 dark:bg-amber-400': browse?.[0] === 'hotel-reviews',
@@ -26,9 +28,7 @@ const Map = ({ data }) => {
           </p>
           <a
             className="
-            px-3 py-2 inline-flex items-center
-            text-sm sm:text-base font-sans hover:underline
-            bg-white bg-opacity-90 text-gray-800 rounded-full shadow-md"
+            px-3 py-2 inline-flex items-center text-sm sm:text-base font-sans hover:underline  rounded-full border border-white/20"
             href={`https://www.google.com/maps/d/viewer?mid=${data.mid}`}>
             <div className="inline-flex mr-2">
               <Image
@@ -52,6 +52,18 @@ const Map = ({ data }) => {
                 src={`https://www.google.com/maps/d/embed?mid=${data.mid}&z=6&hl=en`}
                 title={data.title}
                 width="800"></iframe>
+              <div
+                className={cx(
+                  'absolute inset-0 opacity-0 hover:opacity-100 transition-all duration-300 cursor-pointer flex items-center justify-center text-2xl font-sans font-medium hover:backdrop-blur-sm',
+                  { hidden: mapInteractive },
+                )}
+                onClick={() => setMapInteractive(true)}>
+                <button
+                  className="w-2/3 p-8 text-blue-400 dark:text-white rounded-xl bg-white/75 dark:bg-black/25 shadow-lg"
+                  type="button">
+                  Click to interact with the map
+                </button>
+              </div>
             </div>
           </div>
         </div>

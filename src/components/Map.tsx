fcbe5a503fbc6bfcc9299df3b3be20b4479f8871
Benjamin Knight <ben@benknight.me>
2022-09-26 12:34:27 +0700

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -14,13 +14,18 @@ const Map = ({ data }) => {
       <div className="lg:rounded-lg overflow-hidden">
         <div
           className={cx(
-            'relative py-8 px-6 md:p-8 text-center dark:bg-opacity-25 dark:text-white lg:rounded-t-lg font-display',
+            'relative py-8 px-6 md:p-10 text-center dark:text-white lg:rounded-t-lg font-display',
             {
-              'bg-blue-100 dark:bg-blue-500': browse === 'features-guides',
-              'bg-amber-100 dark:bg-amber-400': browse?.[0] === 'hotel-reviews',
-              'text-white bg-green-600': browse?.[0] === 'motorbike-guides',
-              'text-white bg-yellow-900': browse?.[0] === 'destinations',
-              'bg-yellow-200': browse?.[0] === 'food-and-drink',
+              'bg-indigo-100 dark:bg-indigo-500/20':
+                browse === 'features-guides',
+              'bg-amber-500/40 dark:bg-amber-500/10':
+                browse?.[0] === 'hotel-reviews',
+              'bg-emerald-700/40 dark:bg-emerald-500/20':
+                browse?.[0] === 'motorbike-guides',
+              'bg-yellow-700/40 dark:bg-yellow-800/20':
+                browse?.[0] === 'destinations',
+              'bg-yellow-400/50 dark:bg-yellow-500/10':
+                browse?.[0] === 'food-and-drink',
             },
           )}>
           <h3 className="text-xl md:text-2xl">{data.title}</h3>
@@ -28,8 +33,7 @@ const Map = ({ data }) => {
             {data.description}
           </p>
           <a
-            className="
-            px-3 py-2 inline-flex items-center text-sm sm:text-base font-sans hover:underline  rounded-full border border-white/20"
+            className="px-3 py-2 inline-flex items-center text-sm sm:text-base font-sans hover:underline  rounded-full border border-black/20 dark:border-white/20"
             href={`https://www.google.com/maps/d/viewer?mid=${data.mid}`}>
             <div className="inline-flex mr-2">
               <Image
@@ -46,7 +50,7 @@ const Map = ({ data }) => {
         </div>
         <div className="relative">
           <div className="overflow-hidden">
-            <div className="h-[75vh] mb-[-14px] min-h-[850px]">
+            <div className="h-[95vh] mb-[-14px]">
               <iframe
                 className="absolute inset-0 w-full h-full"
                 height="650"
@@ -55,11 +59,11 @@ const Map = ({ data }) => {
                 width="800"></iframe>
               <div
                 className={cx(
-                  'absolute inset-0 transition-all duration-300 cursor-pointer flex items-center justify-center text-2xl font-sans font-medium backdrop-saturate-50',
+                  'group absolute inset-0 transition-all duration-300 cursor-pointer flex items-center justify-center text-2xl font-sans font-medium backdrop-saturate-50',
                   mapInteractive ? 'hidden' : 'hidden pointer:flex',
                 )}
                 onClick={() => setMapInteractive(true)}>
-                <button className="w-2/3 p-8 text-indigo-400 dark:text-white rounded-xl bg-white/75 dark:bg-black/60 shadow-lg flex items-center justify-center">
+                <button className="flex items-center justify-center w-2/3 p-8 dark:text-white rounded-xl bg-white/75 hover:bg-white/90 dark:bg-black/60 dark:hover:bg-black/75 shadow-lg transition-colors duration-150 text-indigo-400 opacity-0 group-hover:opacity-100">
                   <CursorClickIcon className="w-8 h-8 mr-2" /> Click to interact
                   with the map
                 </button>

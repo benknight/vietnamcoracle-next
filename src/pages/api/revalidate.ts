00a3ade3b788778082c9111cbe81284adf0269cf
Benjamin Knight <ben@benknight.me>
2023-01-21 22:48:47 +0700

    Update revalidate.ts

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,16 +4,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { secret, path } = req.query;
+  const { paths } = req.query;
+  const { secret } = req.body;
   if (secret !== process.env.WORDPRESS_PREVIEW_SECRET) {
-    return res.status(401).json({ message: 'Invalid secret' });
+    return res.status(401).json({ message: 'Invalid request' });
   }
   try {
-    await res.revalidate(String(path));
-    return res.send('OK');
+    await Promise.all(
+      String(paths)
+        .split(',')
+        .map(path => res.revalidate(path)),
+    );
+    return res.send({ success: true });
   } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send('Error revalidating');
+    return res.status(500).send({ success: false });
   }
 }

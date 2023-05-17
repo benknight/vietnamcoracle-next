6cecaeef52453a920e0c0e27dc1f3cf9618c8814
Benjamin Knight <ben@benknight.me>
2023-05-17 18:41:37 +0700

    Update search.tsx

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -190,7 +190,7 @@ export default function Search() {
         <link rel="canonical" href="https://www.vietnamcoracle.com/" />
       </Head>
       <div className="bg-gray-100 dark:bg-transparent">
-        <Layout className="max-w-screen-2xl pb-14 xl:pb-0 bg-white">
+        <Layout className="max-w-screen-2xl pb-14 xl:pb-0 bg-white dark:bg-transparent">
           <LayoutMain className="min-h-screen bg-gray-100 dark:bg-black lg:bg-transparent">
             <div className="px-2 lg:px-8 pb-8">
               {[...Array(pageCount)].map((_item, i) => (

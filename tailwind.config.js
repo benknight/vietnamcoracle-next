654662681dcbbd6f0514c3f38b84ebb9441d24f9
Benjamin Knight <ben@benknight.me>
2023-09-07 11:30:36 +0700

    Remove line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const breakpoints = require('./src/config/breakpoints');
 
 module.exports = {
-  plugins: [require('@tailwindcss/line-clamp')],
+  plugins: [],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {

266e21b62ebd40c223a5217c0651ba386a1ec0e2
Benjamin Knight <ben@benknight.me>
2024-09-01 22:28:32 +0700

    create api function for generating signed download links

---
+++ src/pages/api/download-link.ts
@@ -0,0 +1,54 @@
+import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import Stripe from 'stripe';
+
+const distributionUrl = 'https://d2gs0ocb6y9y9p.cloudfront.net';
+const downloadLimit = Number(process.env.OFFLINE_GUIDE_DOWNLOAD_LIMIT || 100);
+const expiresMs = 60 * 1000; // 1 minutes
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Read the Checkout Session ID from the URL query parameter
+  const paymentIntentId = req.body.pi as string;
+  const fileKey = req.body.file_key as string;
+  const isTest = Boolean(req.body.is_test);
+
+  if (!paymentIntentId) {
+    return res.status(400).send('Payment Intent ID is required');
+  }
+
+  const stripe = new Stripe(
+    isTest ? process.env.STRIPE_SECRET_KEY_TEST : process.env.STRIPE_SECRET_KEY,
+  );
+
+  try {
+    // Retrieve the Payment Intent to get the current download count
+    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+
+    // Increment the download count
+    const currentCount = paymentIntent.metadata.download_count || 0;
+    const newCount = Number(currentCount) + 1;
+
+    // Update the Payment Intent with the new download count
+    await stripe.paymentIntents.update(paymentIntentId, {
+      metadata: { download_count: newCount.toString() },
+    });
+
+    if (newCount > downloadLimit) {
+      res.status(400).send('Download limit exceeded');
+    } else {
+      const url = getSignedUrl({
+        url: `${distributionUrl}/${fileKey.trim()}`,
+        keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
+        privateKey: process.env.CLOUDFRONT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        dateLessThan: new Date(Date.now() + expiresMs).toISOString(),
+      });
+      res.send(url);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Unexpected error');
+  }
+}
--- src/pages/download-guide.tsx
+++ src/pages/download-guide.tsx
@@ -1,40 +1,51 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Stripe from 'stripe';
-import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
 import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
-
-// Configs
-const s3Client = new S3Client({
-  region: 'ap-southeast-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-const bucketName = 'coracle';
-const distributionUrl = 'https://d2gs0ocb6y9y9p.cloudfront.net';
-const downloadLimit = Number(process.env.OFFLINE_GUIDE_DOWNLOAD_LIMIT || 20);
-const expiresMs = 15 * 60 * 1000; // 15 minutes
 
 const isPdf = (name: string) => name.toLowerCase().endsWith('.pdf');
 
-const sortFiles = (a: DownloadLink, b: DownloadLink) => {
+const sortFiles = (a: File, b: File) => {
   if (isPdf(a.name) && !isPdf(b.name)) return -1;
   if (!isPdf(a.name) && isPdf(b.name)) return 1;
   return a.name.localeCompare(b.name);
 };
 
-type DownloadLink = {
+type File = {
   name: string;
-  url: string;
+  key: string;
 };
 
 type Props = {
-  downloadLinks?: DownloadLink[];
+  files?: File[];
+  isTest?: boolean;
   error?: string;
+  paymentIntentId?: string;
 };
 
 export default function DownloadGuide(props: Props) {
+  const onClick = async (fileKey: string) => {
+    const response = await fetch('/api/download-link', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        file_key: fileKey,
+        is_test: props.isTest,
+        pi: props.paymentIntentId,
+      }),
+    });
+
+    const text = await response.text();
+
+    if (response.status !== 200) {
+      alert(`${text}. Please contact admin@vietnamcoracle.com for assistance.`);
+      return;
+    }
+
+    window.open(text, '_blank');
+  };
+
   if (props.error) {
     return (
       <div className="page-wrap text-center pt-48">
@@ -42,16 +53,20 @@ export default function DownloadGuide(props: Props) {
       </div>
     );
   }
+
   return (
     <div className="page-wrap flex justify-center items-center pt-48 pb-12">
       <div className="max-w-screen-sm w-full">
         <h2 className="text-2xl font-bold mb-4 text-center">Download Files</h2>
         <ul className="space-y-2">
-          {props.downloadLinks.sort(sortFiles).map(link => (
-            <li key={link.name}>
-              <a href={link.url} download={link.name} className="btn w-full">
-                {link.name}
-              </a>
+          {props.files.sort(sortFiles).map(file => (
+            <li key={file.name}>
+              <button
+                type="button"
+                onClick={() => onClick(file.key)}
+                className="btn w-full">
+                {file.name}
+              </button>
             </li>
           ))}
         </ul>
@@ -65,6 +80,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 ) => {
   const { query } = context;
   const sessionId = query['checkout_session_id'] as string;
+  const isTest = sessionId.startsWith('cs_test');
 
   if (!sessionId) {
     return {
@@ -75,22 +91,27 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
   }
 
   const stripe = new Stripe(
-    sessionId.startsWith('cs_test')
-      ? process.env.STRIPE_SECRET_KEY_TEST
-      : process.env.STRIPE_SECRET_KEY,
+    isTest ? process.env.STRIPE_SECRET_KEY_TEST : process.env.STRIPE_SECRET_KEY,
   );
 
-  // Retrieve the session from Stripe
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['payment_link'],
   });
 
-  // Extract the Payment Link ID from the Checkout Session
   const paymentLink = session.payment_link as Stripe.PaymentLink;
+  const paymentIntentId = session.payment_intent as string;
   const s3Prefix = paymentLink.metadata.s3_prefix;
 
+  const s3Client = new S3Client({
+    region: 'ap-southeast-1',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
   const listCommand = new ListObjectsV2Command({
-    Bucket: bucketName,
+    Bucket: 'coracle',
     Prefix: s3Prefix,
   });
 
@@ -110,48 +131,14 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 
   fileKeys.push('Offline Maps How-to Guide.pdf');
 
-  // Retrieve the Payment Intent to get the current download count
-  const paymentIntentId = session.payment_intent as string;
-
-  // Retrieve the Payment Intent to get the current download count
-  const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
-
-  // Increment the download count
-  const currentCount = paymentIntent.metadata.download_count || 0;
-  const newCount = Number(currentCount) + 1;
-
-  // Update the Payment Intent with the new download count
-  await stripe.paymentIntents.update(paymentIntentId, {
-    metadata: { download_count: newCount.toString() },
-  });
-
-  if (newCount > downloadLimit) {
-    return {
-      props: {
-        error: `Download link expired for checkout session ID ${sessionId}.`,
-      },
-    };
-  }
-
-  const downloadLinks: DownloadLink[] = [];
-
-  for (const fileKey of fileKeys) {
-    const signedUrl = getSignedUrl({
-      url: `${distributionUrl}/${fileKey.trim()}`,
-      keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
-      privateKey: process.env.CLOUDFRONT_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      dateLessThan: new Date(Date.now() + expiresMs).toISOString(),
-    });
-
-    downloadLinks.push({
-      name: fileKey.replace(`${s3Prefix}/`, '').trim(),
-      url: signedUrl,
-    });
-  }
-
   return {
     props: {
-      downloadLinks,
+      files: fileKeys.map(key => ({
+        name: key.replace(`${s3Prefix}/`, '').trim(),
+        key,
+      })),
+      isTest,
+      paymentIntentId,
     },
   };
 };

7159ffc5a04f0521167c87b81fc2c7c06ea686a9
Benjamin Knight <ben@benknight.me>
2025-01-09 17:58:15 +0700

    remove patreon learn more link

--- src/components/PatronOnlyContentGate.tsx
+++ src/components/PatronOnlyContentGate.tsx
@@ -84,11 +84,6 @@ export default function PatronOnlyContentGate({ patron, patreonLevel }: Props) {
             />
             Login with Patreon
           </a>
-          <p className="my-8 text-xs">
-            <Link href="/become-a-patron-of-vietnam-coracle">
-              <a className="link">Learn More about Patreon</a>
-            </Link>
-          </p>
         </div>
       )}
     </div>

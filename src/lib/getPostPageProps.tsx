7d1ccea23358d40aa9e17ea6d7ee35cf2215c063
Benjamin Knight <ben@benknight.me>
2025-03-18 22:25:48 +0700

    refactor: getPostPageProps improvements

--- src/lib/getPostPageProps.tsx
+++ src/lib/getPostPageProps.tsx
@@ -1,24 +1,10 @@
 import * as cheerio from 'cheerio';
 import { differenceInMonths, parse } from 'date-fns';
-import ReactDOMServer from 'react-dom/server';
-import OldPostAlert from '../components/OldPostAlert';
 import cleanPostHTML from './cleanPostHTML';
 import cmsToNextUrls from './cmsToNextUrls';
+import getOldPostAlertHtml from './getOldPostAlertHtml';
 
-type NavLinks = string[][];
-
-export async function getPostPageProps(
-  data: any,
-  preview: boolean,
-): Promise<{
-  ads: any;
-  data: any;
-  html: string;
-  monthsOld: number;
-  navCategory?: string;
-  postNav: any;
-  preview: boolean;
-}> {
+export async function getPostPageProps(data: any, preview: boolean) {
   if (preview) {
     if (data.contentNode) {
       data.contentNode = {
@@ -28,119 +14,125 @@ export async function getPostPageProps(
     }
   }
 
-  let postNav: NavLinks = null;
-  let html = '';
-  let monthsOld: number = null;
+  let postNav: string[][] = [];
+  let monthsOld: number = -1;
 
-  if (data.contentNode?.content) {
-    html = cleanPostHTML(data.contentNode.content);
+  if (!data.contentNode?.content) {
+    throw new Error('No post content found');
   }
 
-  if (html) {
-    const $ = cheerio.load(html);
-    const lastUpdated = $(
-      "p:first-of-type:contains('Last updated'), p:first-of-type:contains('Last Updated'), p:first-of-type:contains('First published'), p:first-of-type:contains('First Published')",
-    );
+  let html = cleanPostHTML(data.contentNode.content);
 
+  const $ = cheerio.load(html);
+
+  const lastUpdated = $(
+    "p:first-of-type:contains('Last updated'), p:first-of-type:contains('Last Updated'), p:first-of-type:contains('First published'), p:first-of-type:contains('First Published')",
+  );
+
+  if (lastUpdated.length > 0) {
+    lastUpdated.addClass('!font-display text-sm !my-4 !m-0 !text-left');
+    const date = lastUpdated
+      .text()
+      .match(/(Last\s+updated|First\s+published)\s+([^|]+)/i)?.[2]
+      ?.trim()
+      .replace(/\s+/g, ' ');
+    if (date) {
+      const parsed = parse(date, 'LLLL yyyy', new Date());
+      monthsOld = differenceInMonths(new Date(), parsed);
+      if (monthsOld > 36) {
+        $(getOldPostAlertHtml(monthsOld)).insertAfter(lastUpdated);
+      }
+    }
+  }
+
+  if (data.contentNode.status === 'publish') {
+    const html = '<div class="mt-4 mb-8"><share-buttons /></div>';
     if (lastUpdated.length > 0) {
-      lastUpdated.addClass('!font-display text-sm !my-4 !m-0 !text-left');
-      const date = lastUpdated
-        .text()
-        .match(/(Last\s+updated|First\s+published)\s+([^|]+)/i)?.[2]
-        ?.trim()
-        .replace(/\s+/g, ' ');
-      if (date) {
-        const parsed = parse(date, 'LLLL yyyy', new Date());
-        monthsOld = differenceInMonths(new Date(), parsed);
-        if (monthsOld > 36) {
-          $(
-            ReactDOMServer.renderToStaticMarkup(
-              <OldPostAlert className="mb-6 lg:mb-8" monthsOld={monthsOld} />,
-            ),
-          ).insertAfter(lastUpdated);
-        }
-      }
+      $(html).insertAfter(lastUpdated);
+    } else {
+      $.root().prepend(html);
     }
+  }
 
-    if (data.contentNode.status === 'publish') {
-      const html = '<div class="mt-4 mb-8"><share-buttons /></div>';
-      if (lastUpdated.length > 0) {
-        $(html).insertAfter(lastUpdated);
-      } else {
-        $.root().prepend(html);
-      }
-    }
+  // Pass post data to share buttons
+  $('share-buttons').attr({
+    'data-title': data.contentNode.title,
+    'data-link': cmsToNextUrls(data.contentNode.link),
+    'data-image': data.contentNode.featuredImage?.node.sourceUrl ?? '',
+  });
 
-    // Pass post data to share buttons
-    $('share-buttons').attr({
-      'data-title': data.contentNode.title,
-      'data-link': cmsToNextUrls(data.contentNode.link),
-      'data-image': data.contentNode.featuredImage?.node.sourceUrl ?? '',
+  // Lazy load all iframes
+  $('iframe').attr({ loading: 'lazy' });
+
+  // Add overlay to Google Maps embeds
+  $('iframe')
+    .filter(function () {
+      return /(google(\.[\w]+)+\/maps[\/\w-\.]+\/embed([\?&][\w-\.]+=[\w-\.]+)+)/g.test(
+        $(this).attr('src') || '',
+      );
+    })
+    .replaceWith(function () {
+      return $('<map-overlay />').attr({
+        'data-height': $(this).attr('height') || '',
+        'data-src': $(this).attr('src') || '',
+        'data-title': $(this).attr('title') || '',
+        'data-width': $(this).attr('width') || '',
+      });
     });
 
-    // Lazy load all iframes
-    $('iframe').attr({ loading: 'lazy' });
+  // Generate contents menu
+  const internalLinks = $(
+    data.contentNode?.settings?.useNextStyles
+      ? '.wp-block-buttons:first-of-type .wp-block-button__link'
+      : 'h2 strong a',
+  );
 
-    // Add overlay to Google Maps embeds
-    $('iframe')
-      .filter(function () {
-        return /(google(\.[\w]+)+\/maps[\/\w-\.]+\/embed([\?&][\w-\.]+=[\w-\.]+)+)/g.test(
-          $(this).attr('src'),
-        );
-      })
-      .replaceWith(function () {
-        return $('<map-overlay />').attr({
-          'data-height': $(this).attr('height'),
-          'data-src': $(this).attr('src'),
-          'data-title': $(this).attr('title') ?? '',
-          'data-width': $(this).attr('width'),
-        });
-      });
+  if (internalLinks.length) {
+    postNav = [
+      ...internalLinks
+        .toArray()
+        .map(element => [
+          $(element).attr('href') || '',
+          $(element).text() || '',
+        ]),
+    ];
+  }
 
-    // Generate contents menu
-    const internalLinks = $(
-      data.contentNode?.settings?.useNextStyles
-        ? '.wp-block-buttons:first-of-type .wp-block-button__link'
-        : 'h2 strong a',
-    );
+  // Remove "Back Top" link after related posts
+  $('related-posts')?.parent().next('p:has(a[href="#top"])').remove();
 
-    if (internalLinks.length) {
-      postNav = [
-        ...internalLinks
-          .toArray()
-          .map(element => [$(element).attr('href'), $(element).text()]),
-      ];
-    }
+  // Put agoda inside iframe
+  $('div[id^="adgshp"]').each((_i, element) => {
+    const scriptTags = [
+      ...$(element).nextAll('script').toArray(),
+      ...$(element).next('p').find('script').toArray(),
+    ];
 
-    // Remove "Back Top" link after related posts
-    $('related-posts')?.parent().next('p:has(a[href="#top"])').remove();
-
-    // Put agoda inside iframe
-    $('div[id^="adgshp"]').each((_i, element) => {
-      const scriptTags = [
-        ...$(element).nextAll('script').toArray(),
-        ...$(element).next('p').find('script').toArray(),
-      ];
-      const html = $('<div />')
+    const html =
+      $('<div />')
         .append($(element).clone(), $(scriptTags).clone())
         .html()
-        .replace('<br />', '');
-      $(element).replaceWith(
-        `<iframe src="/api/iframe-service/?html=${encodeURIComponent(
-          html,
-        )}" height="500" title="Book your accommodation"></iframe>`,
-      );
-      $(scriptTags).remove();
-    });
+        ?.replace('<br />', '') ?? '';
 
-    // Fix captions
-    $('p > a[href$=".jpg"]:first-child').each((_i, element) => {
-      $(element.parent).addClass('text-sm text-center legacy-caption mb-8');
-      $(element).addClass('post-image').insertBefore(element.parent);
-    });
+    $(element).replaceWith(
+      `<iframe src="/api/iframe-service/?html=${encodeURIComponent(
+        html,
+      )}" height="500" title="Book your accommodation"></iframe>`,
+    );
 
-    html = $.html();
-  }
+    $(scriptTags).remove();
+  });
+
+  // Fix captions
+  $('p > a[href$=".jpg"]:first-child').each((_i, element) => {
+    if (!element.parent) {
+      return;
+    }
+    $(element.parent).addClass('text-sm text-center legacy-caption mb-8');
+    $(element).addClass('post-image').insertBefore(element.parent);
+  });
+
+  html = $.html();
 
   return {
     ads: preview

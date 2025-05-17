f3cffaff8166152b63acb827b56fec651fbba9a7
Benjamin Knight <ben@benknight.me>
2025-05-17 14:54:58 +0700

    switch to edge runtime for menu route

--- src/pages/api/menu.ts
+++ src/pages/api/menu.ts
@@ -1,9 +1,13 @@
 import { gql } from 'graphql-request';
-import type { NextApiRequest, NextApiResponse } from 'next';
 import GraphQLClient from '../../lib/WPGraphQLClient';
 
-export default async function menu(_req: NextApiRequest, res: NextApiResponse) {
-  const api = new GraphQLClient();
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler() {
+  const api = new GraphQLClient('admin');
+
   const result = await api.request(gql`
     query Menu {
       menuItems(where: { location: HEADER_MENU_NEXT }, first: 1000) {
@@ -17,10 +21,14 @@ export default async function menu(_req: NextApiRequest, res: NextApiResponse) {
       }
     }
   `);
+
   const maxAgeSeconds = 60 * 60 * 24; // 24 hours
-  res.setHeader(
-    'Cache-Control',
-    `public, max-age=0, s-maxage=${maxAgeSeconds}`,
-  );
-  res.send(result);
+
+  return new Response(JSON.stringify(result), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': `public, max-age=0, s-maxage=${maxAgeSeconds}`,
+    },
+  });
 }

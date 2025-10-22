e777e6e522ce235dd5caa4b3ba88464076aac6bd
Benjamin Knight <ben@benknight.me>
2025-10-22 17:18:08 +0700

    more header ad fixes

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -34,7 +34,7 @@ export default function Post(props: Props) {
         />
       )} */}
       {post.ads?.header?.enabled && (
-        <div className="bg-gray-300 dark:bg-gray-800 overflow-hidden">
+        <div className="bg-gray-800 overflow-hidden">
           <div
             className="w-full h-full header-ad-container"
             dangerouslySetInnerHTML={{

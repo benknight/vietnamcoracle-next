42364d1c6093f7b03791b346ed371bd69a2ec5c8
Benjamin Knight <ben@benknight.me>
2025-11-15 17:25:07 +0700

    fix build error hopefully

---
+++ src/app/download-guide/components/DownloadGuideTreeView.tsx
@@ -0,0 +1,169 @@
+'use client';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+
+interface File {
+  name: string;
+  key: string;
+}
+
+interface DownloadGuideTreeViewProps {
+  groups: { [key: string]: File[] };
+  isTest: boolean;
+  paymentIntentId: string;
+}
+
+export default function DownloadGuideTreeView({
+  groups,
+  isTest,
+  paymentIntentId,
+}: DownloadGuideTreeViewProps) {
+  return (
+    <SimpleTreeView
+      className="bg-black/5 dark:bg-white/5 rounded py-2"
+      aria-label="file system navigator">
+      {Object.keys(groups).map(prefix => {
+        const sortedFiles = groups[prefix].sort(sortFiles);
+        const guideName =
+          prefix === 'Offline Maps How-to Guide.pdf'
+            ? 'Offline Maps How-to Guide'
+            : sortedFiles[0].name.replace('.pdf', '');
+
+        const pdfFiles = sortedFiles.filter(file => isPdf(file.name));
+        const mapFiles = sortedFiles.filter(
+          file => isKmz(file.name) || isGpx(file.name),
+        );
+        const gpxFiles = mapFiles.filter(file => isGpx(file.name));
+        const kmzFiles = mapFiles.filter(file => isKmz(file.name));
+
+        return (
+          <TreeItem
+            key={prefix}
+            itemId={prefix}
+            label={guideName}
+            className="text-lg mt-4 mb-2 font-medium !font-sans">
+            {pdfFiles.map(file => {
+              const urlParams = new URLSearchParams({
+                file_key: file.key,
+                is_test: isTest ? '1' : '0',
+                pi: paymentIntentId,
+              });
+              return (
+                <TreeItem
+                  key={file.name}
+                  itemId={`${file.key}-item`} // Unique itemId
+                  label={
+                    <a
+                      className="hover:underline"
+                      href={`/api/download-link?${urlParams.toString()}`}
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      {file.name}
+                    </a>
+                  }
+                />
+              );
+            })}
+            {mapFiles.length > 0 && (
+              <TreeItem
+                key={`${prefix}-map-files`}
+                itemId={`${prefix}-map-files`}
+                label="Map Files"
+                className="!font-sans">
+                {gpxFiles.length > 0 && (
+                  <TreeItem
+                    key={`${prefix}-map-files-gpx`}
+                    itemId={`${prefix}-map-files-gpx`}
+                    label="GPX">
+                    {gpxFiles.map(file => {
+                      const urlParams = new URLSearchParams({
+                        file_key: file.key,
+                        is_test: isTest ? '1' : '0',
+                        pi: paymentIntentId,
+                      });
+                      return (
+                        <TreeItem
+                          key={file.name}
+                          itemId={`${file.key}-item`} // Unique itemId
+                          label={
+                            <a
+                              className="hover:underline"
+                              href={`/api/download-link?${urlParams.toString()}`}
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              {file.name}
+                            </a>
+                          }
+                        />
+                      );
+                    })}
+                  </TreeItem>
+                )}
+                {kmzFiles.length > 0 && (
+                  <TreeItem
+                    key={`${prefix}-map-files-kmz`}
+                    itemId={`${prefix}-map-files-kmz`}
+                    label="KMZ">
+                    {kmzFiles.map(file => {
+                      const urlParams = new URLSearchParams({
+                        file_key: file.key,
+                        is_test: isTest ? '1' : '0',
+                        pi: paymentIntentId,
+                      });
+                      return (
+                        <TreeItem
+                          className="even:bg-black/5 even:dark:bg-white/5"
+                          key={file.name}
+                          itemId={`${file.key}-item`} // Unique itemId
+                          label={
+                            <a
+                              className="hover:underline"
+                              href={`/api/download-link?${urlParams.toString()}`}
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              {file.name}
+                            </a>
+                          }
+                        />
+                      );
+                    })}
+                  </TreeItem>
+                )}
+              </TreeItem>
+            )}
+          </TreeItem>
+        );
+      })}
+    </SimpleTreeView>
+  );
+}
+function isPdf(name: string) {
+  return name.toLowerCase().endsWith('.pdf');
+}
+
+function isKmz(name: string) {
+  return name.toLowerCase().endsWith('.kmz');
+}
+
+function isGpx(name: string) {
+  return name.toLowerCase().endsWith('.gpx');
+}
+
+function sortFiles(a: File, b: File) {
+  const aPrefix = a.key.split('/')[0];
+  const bPrefix = b.key.split('/')[0];
+
+  if (aPrefix !== bPrefix) {
+    return aPrefix.localeCompare(bPrefix);
+  }
+
+  // Assign a priority to each file type: PDF (0), KMZ (1), GPX (2), others (3)
+  const getPriority = (name: string) =>
+    isPdf(name) ? 0 : isKmz(name) ? 1 : isGpx(name) ? 2 : 3;
+
+  const priorityDiff = getPriority(a.name) - getPriority(b.name);
+
+  if (priorityDiff !== 0) return priorityDiff;
+
+  return a.name.localeCompare(b.name);
+}
--- src/app/download-guide/page.tsx
+++ src/app/download-guide/page.tsx
@@ -3,8 +3,7 @@ import Stripe from 'stripe';
 import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
 import DownloadGuideError from './components/DownloadGuideError';
 import DownloadGuideSelection from './components/DownloadGuideSelection';
-import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import DownloadGuideTreeView from './components/DownloadGuideTreeView';
 
 interface File {
   name: string;
@@ -16,137 +15,6 @@ interface Props {
     checkout_session_id?: string;
     guides_choice?: string;
   }>;
-}
-
-interface DownloadGuideTreeViewProps {
-  groups: { [key: string]: File[] };
-  isTest: boolean;
-  paymentIntentId: string;
-}
-
-function DownloadGuideTreeView({
-  groups,
-  isTest,
-  paymentIntentId,
-}: DownloadGuideTreeViewProps) {
-  return (
-    <SimpleTreeView
-      className="bg-black/5 dark:bg-white/5 rounded py-2"
-      aria-label="file system navigator">
-      {Object.keys(groups).map(prefix => {
-        const sortedFiles = groups[prefix].sort(sortFiles);
-        const guideName =
-          prefix === 'Offline Maps How-to Guide.pdf'
-            ? 'Offline Maps How-to Guide'
-            : sortedFiles[0].name.replace('.pdf', '');
-
-        const pdfFiles = sortedFiles.filter(file => isPdf(file.name));
-        const mapFiles = sortedFiles.filter(
-          file => isKmz(file.name) || isGpx(file.name),
-        );
-        const gpxFiles = mapFiles.filter(file => isGpx(file.name));
-        const kmzFiles = mapFiles.filter(file => isKmz(file.name));
-
-        return (
-          <TreeItem
-            key={prefix}
-            itemId={prefix}
-            label={guideName}
-            className="text-lg mt-4 mb-2 font-medium !font-sans">
-            {pdfFiles.map(file => {
-              const urlParams = new URLSearchParams({
-                file_key: file.key,
-                is_test: isTest ? '1' : '0',
-                pi: paymentIntentId,
-              });
-              return (
-                <TreeItem
-                  key={file.name}
-                  itemId={`${file.key}-item`} // Unique itemId
-                  label={
-                    <a
-                      className="hover:underline"
-                      href={`/api/download-link?${urlParams.toString()}`}
-                      target="_blank"
-                      rel="noopener noreferrer">
-                      {file.name}
-                    </a>
-                  }
-                />
-              );
-            })}
-            {mapFiles.length > 0 && (
-              <TreeItem
-                key={`${prefix}-map-files`}
-                itemId={`${prefix}-map-files`}
-                label="Map Files"
-                className="!font-sans">
-                {gpxFiles.length > 0 && (
-                  <TreeItem
-                    key={`${prefix}-map-files-gpx`}
-                    itemId={`${prefix}-map-files-gpx`}
-                    label="GPX">
-                    {gpxFiles.map(file => {
-                      const urlParams = new URLSearchParams({
-                        file_key: file.key,
-                        is_test: isTest ? '1' : '0',
-                        pi: paymentIntentId,
-                      });
-                      return (
-                        <TreeItem
-                          key={file.name}
-                          itemId={`${file.key}-item`} // Unique itemId
-                          label={
-                            <a
-                              className="hover:underline"
-                              href={`/api/download-link?${urlParams.toString()}`}
-                              target="_blank"
-                              rel="noopener noreferrer">
-                              {file.name}
-                            </a>
-                          }
-                        />
-                      );
-                    })}
-                  </TreeItem>
-                )}
-                {kmzFiles.length > 0 && (
-                  <TreeItem
-                    key={`${prefix}-map-files-kmz`}
-                    itemId={`${prefix}-map-files-kmz`}
-                    label="KMZ">
-                    {kmzFiles.map(file => {
-                      const urlParams = new URLSearchParams({
-                        file_key: file.key,
-                        is_test: isTest ? '1' : '0',
-                        pi: paymentIntentId,
-                      });
-                      return (
-                        <TreeItem
-                          className="even:bg-black/5 even:dark:bg-white/5"
-                          key={file.name}
-                          itemId={`${file.key}-item`} // Unique itemId
-                          label={
-                            <a
-                              className="hover:underline"
-                              href={`/api/download-link?${urlParams.toString()}`}
-                              target="_blank"
-                              rel="noopener noreferrer">
-                              {file.name}
-                            </a>
-                          }
-                        />
-                      );
-                    })}
-                  </TreeItem>
-                )}
-              </TreeItem>
-            )}
-          </TreeItem>
-        );
-      })}
-    </SimpleTreeView>
-  );
 }
 
 export default async function DownloadGuide({ searchParams }: Props) {
@@ -181,11 +49,7 @@ export default async function DownloadGuide({ searchParams }: Props) {
       expand: ['payment_link', 'payment_intent'],
     });
   } catch (error) {
-    return {
-      props: {
-        error: `Failed to retrieve checkout session ${sessionId}.`,
-      },
-    };
+    throw new Error(`Failed to retrieve checkout session ${sessionId}.`);
   }
 
   let resolvedS3Prefix: string;
@@ -338,35 +202,4 @@ export default async function DownloadGuide({ searchParams }: Props) {
       </div>
     </div>
   );
-}
-
-function isPdf(name: string) {
-  return name.toLowerCase().endsWith('.pdf');
-}
-
-function isKmz(name: string) {
-  return name.toLowerCase().endsWith('.kmz');
-}
-
-function isGpx(name: string) {
-  return name.toLowerCase().endsWith('.gpx');
-}
-
-function sortFiles(a: File, b: File) {
-  const aPrefix = a.key.split('/')[0];
-  const bPrefix = b.key.split('/')[0];
-
-  if (aPrefix !== bPrefix) {
-    return aPrefix.localeCompare(bPrefix);
-  }
-
-  // Assign a priority to each file type: PDF (0), KMZ (1), GPX (2), others (3)
-  const getPriority = (name: string) =>
-    isPdf(name) ? 0 : isKmz(name) ? 1 : isGpx(name) ? 2 : 3;
-
-  const priorityDiff = getPriority(a.name) - getPriority(b.name);
-
-  if (priorityDiff !== 0) return priorityDiff;
-
-  return a.name.localeCompare(b.name);
 }
